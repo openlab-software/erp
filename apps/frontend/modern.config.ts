b3fb6ffad1c23882d91084c2bdc19524cda159b2
patrickdevbr <patrickribeiro.rb@gmail.com>
2026-04-02 16:44:46 -0300

    modern.js build test

--- apps/frontend/modern.config.ts
+++ apps/frontend/modern.config.ts
@@ -3,4 +3,7 @@ import { appTools, defineConfig } from '@modern-js/app-tools';
 // https://modernjs.dev/en/configure/app/usage
 export default defineConfig({
   plugins: [appTools()],
+  server: {
+    ssr: true,
+  },
 });
